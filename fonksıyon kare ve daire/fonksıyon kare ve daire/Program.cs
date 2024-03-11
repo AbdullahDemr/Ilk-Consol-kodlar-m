a47ca4fbffd6bcb379898f2ebc1a1e894a4a7ace
AbdullahDemr <138607854+AbdullahDemr@users.noreply.github.com>
2024-03-12 02:54:45 +0300

    Add files via upload

---
+++ fonksıyon kare ve daire/fonksıyon kare ve daire/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("İki farklı rakam girin:");
+
+        Console.Write("1. rakamı girin: ");
+        double sayi1 = Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("2. rakamı girin: ");
+        double sayi2 = Convert.ToDouble(Console.ReadLine());
+
+       
+        double dikdortgenAlan = sayi1 * sayi2;
+        Console.WriteLine("Dikdörtgenin Alanı: " + dikdortgenAlan);
+
+        
+        double daireAlan = Math.PI * Math.Pow(sayi1, 2);
+        Console.WriteLine("Dairenin Alanı: " + daireAlan);
+    }
+}
