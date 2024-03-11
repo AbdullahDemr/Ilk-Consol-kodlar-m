4893a6a65b95fb67506c9fdf7bab956bdca96a5e
AbdullahDemr <138607854+AbdullahDemr@users.noreply.github.com>
2024-03-12 02:25:20 +0300

    Add files via upload

---
+++ farklıGezegenlerdeagırlıkhesaplama/agırlık/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("kilo1");
+float kilo1 = Convert.ToSingle(Console.ReadLine());
+float ortalama1;
+float ortalama2;
+
+
+
+ortalama1 = (float)(kilo1 * 0.377);
+Console.WriteLine($"mars agırlıgınız {ortalama1}");
+ortalama2 = (float)(kilo1 * 2.36);
+Console.WriteLine($"jütiper agırlıgınız {ortalama2}");
