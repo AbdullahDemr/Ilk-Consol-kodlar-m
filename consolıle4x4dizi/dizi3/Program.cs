c67cca91a3ed7c360bd7eb9e5264769abc10beaf
AbdullahDemr <138607854+AbdullahDemr@users.noreply.github.com>
2024-03-12 02:51:35 +0300

    Add files via upload

---
+++ consolıle4x4dizi/dizi3/Program.cs
@@ -0,0 +1,22 @@
+﻿int[,] matrix = new int[4, 4];
+Console.WriteLine("Lütfen 4x4 matrisin elemanlarını giriniz:");
+for (int i = 0; i < 4; i++)
+{
+    string[] row = Console.ReadLine().Split(" ");
+    for (int j = 0; j < 4; j++)
+    {
+        matrix[i, j] = int.Parse(row[j]);
+    }
+}
+int maxElement = matrix[0, 0];
+for (int i = 0; i < 4; i++)
+{
+    for (int j = 0; j < 4; j++)
+    {
+        if (matrix[i, j] > maxElement)
+        {
+            maxElement = matrix[i, j];
+        }
+    }
+}
+Console.WriteLine("Matrisin en büyük elemanı: " + maxElement);
