cc26d510e9242d73183abdec2a8fc1c9e45d6a51
AbdullahDemr <138607854+AbdullahDemr@users.noreply.github.com>
2024-03-12 02:27:08 +0300

    Add files via upload

---
+++ 3basamaklisayininrakmamlarinitoplama/3 basamaklı/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.WriteLine("3 basamaklı bir sayı giriniz: ");
+int sayi = Convert.ToInt32(Console.ReadLine());
+int sayi1 = sayi / 100;
+int sayi2 = sayi / 10 % 10;
+int sayi3 = sayi % 10;
+int toplam = sayi1 + sayi2 + sayi3;
+Console.WriteLine("Girdiğiniz sayının rakamlarının toplamı: " + toplam);
