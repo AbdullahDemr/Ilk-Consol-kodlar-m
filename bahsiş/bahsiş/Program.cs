a56240a0db3c698b3ba0012cb2efc214f9fb75fb
AbdullahDemr <138607854+AbdullahDemr@users.noreply.github.com>
2024-03-12 02:30:51 +0300

    Add files via upload

---
+++ bahsiş/bahsiş/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+
+    {
+    //    string Name = "abdullah";
+    //    Name.Remove(5);
+    //    Console.WriteLine(Name);
+
+        Console.WriteLine("Hesabı girin:");
+        int num1 = Convert.ToInt32(Console.ReadLine());
+
+    Console.WriteLine("Bahsis tutarını girin:");
+        int num2 = Convert.ToInt32(Console.ReadLine());
+
+    Console.WriteLine("Kişi sayısını girin:");
+        int num3 = Convert.ToInt32(Console.ReadLine());
+
+    float hesap = Hesaplama(num1, num2, num3);
+    Console.WriteLine("Hesap: " + hesap);
+
+
+      
+
+
+
+    }
+
+static float Hesaplama(int num1, int num2 = 10, int num3 = 1)
+{
+    int hesap = (num1 * num2 * num3) / 100;
+    hesap += num1;
+
+    return hesap;
+}
+    
+        
+}
